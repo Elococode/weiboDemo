5e728dcd2b0a638465c42ea9d939d06c7d75503a
Ethan Lo <ethanlobot@yahoo.com>
2023-01-06 14:10:05 +0800

    layouts

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>关于</title>
-</head>
-<body>
+@extends('layouts.default')
+
+@section('title','关于')
+
+@section('content')
     <h1>关于</h1>
-</body>
-</html>
+@stop
