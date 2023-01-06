9b7d8d427bcf544a62a1eec1cabea9a6b96d18ab
Ethan Lo <ethanlobot@yahoo.com>
2023-01-06 18:39:38 +0800

    header and footer

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,23 +8,16 @@
     <title>@yield('title','WeiboApp') - Laravel newer!</title>
     <link rel="stylesheet" class="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a href="/" class="navbar-brand">WeiboApp</a>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item">
-                    <a href="/help" class="nav-link">帮助</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">登陆</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    @include('layouts._header')
 
     <div class="container">
-        @yield('content')
+        <div class="offset-md-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
     </div>
 </body>
+
 </html>
