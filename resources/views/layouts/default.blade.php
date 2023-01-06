48cda702ada51cc5095fa3b685c9e3e5f459e379
Ethan Lo <ethanlobot@yahoo.com>
2023-01-06 15:29:48 +0800

    init style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title','WeiboApp')</title>
+    <title>@yield('title','WeiboApp') - Laravel newer!</title>
+    <link rel="stylesheet" href="/css/app.css" class="stylesheet">
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">WeiboApp</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item">
+                    <a href="/help" class="nav-link">帮助</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">登陆</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
