081bfca6c68c7c9f6aa6e92aa18db96900638a95
Ethan Lo <ethanlobot@yahoo.com>
2023-01-09 00:33:34 +0800

    bug fix

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use DateTime;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -22,15 +23,14 @@ class UsersController extends Controller
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password' => 'required|confirmed|min:6'
+            'password' => 'required|confirmed|min:6',
         ]);
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
-        
+
         // 之后我们可以使用 session()->get('success') 通过键名来取出对应会话中的数据，取出的结果为 欢迎，您将在这里开启一段新的旅程~。
         session()->flash('success', '欢迎，您将在这里开启一段新旅程！');
 
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title','WeiboApp') - Laravel newer!</title>
+    <title>@yield('title', 'WeiboApp') - Laravel newer!</title>
     <link rel="stylesheet" class="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
