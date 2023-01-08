79a648b1a16758c72e36c77ae71fd1e306aaba12
Ethan Lo <ethanlobot@yahoo.com>
2023-01-08 17:23:26 +0800

    finish register

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,6 +24,16 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        
+        // 之后我们可以使用 session()->get('success') 通过键名来取出对应会话中的数据，取出的结果为 欢迎，您将在这里开启一段新的旅程~。
+        session()->flash('success', '欢迎，您将在这里开启一段新旅程！');
+
+        return redirect()->route('users.show', [$user]);
     }
 }
