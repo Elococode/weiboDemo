fce373c6a8080aacc873052c076c88a4528008be
Ethan Lo <ethanlobot@yahoo.com>
2023-01-09 18:58:11 +0800

    edit user info

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,7 +22,7 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required|unique:users|max:50',
+            'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6',
         ]);
@@ -36,6 +36,29 @@ class UsersController extends Controller
         // 之后我们可以使用 session()->get('success') 通过键名来取出对应会话中的数据，取出的结果为 欢迎，您将在这里开启一段新的旅程~。
         session()->flash('success', '欢迎，您将在这里开启一段新旅程！');
 
-        return redirect()->route('users.show', [$user]);
+        return redirect()->route('users.show', $user);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6',
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '您的信息已修改成功！');
+        return redirect()->route('users.show', $user);
     }
 }
